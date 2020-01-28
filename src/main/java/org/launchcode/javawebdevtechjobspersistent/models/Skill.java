474e837e54a248173798135d68c3ea62035aef46
Gerard Darris <darrisgr@gmail.com>
2020-01-27 19:52:55 -0600

    Updated Skill.java and SkillController.java

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -13,4 +13,11 @@ public class Skill extends AbstractEntity {
 
     public Skill() {}
 
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
