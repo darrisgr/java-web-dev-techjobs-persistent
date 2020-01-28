7dd70aa20af1ace35db3b7c5f262790919446d42
Gerard Darris <darrisgr@gmail.com>
2020-01-27 18:27:41 -0600

    Updated AbstractEntity.java

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/AbstractEntity.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/AbstractEntity.java
@@ -3,6 +3,8 @@ package org.launchcode.javawebdevtechjobspersistent.models;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Objects;
 
 @MappedSuperclass
@@ -12,6 +14,8 @@ public abstract class AbstractEntity {
     @GeneratedValue
     private int id;
 
+    @NotBlank(message = "Name is required!")
+    @Size(min = 2, max = 255, message = "Name must be between 2 and 255 characters!")
     private String name;
 
     public int getId() {
