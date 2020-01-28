6a88c7f4b8c2cd31ad0688ee1289b7793a04af0c
Gerard Darris <darrisgr@gmail.com>
2020-01-27 19:14:18 -0600

    Updated Employer.java and Skill.java

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,5 +1,16 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Skill extends AbstractEntity {
+
+    @NotNull
+    @Size(max = 500, message = "Must be no greater than 500 characters!")
+    private String description;
+
+    public Skill() {}
 
 }
