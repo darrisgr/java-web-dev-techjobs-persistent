cf9f3331af454515f5ba2419bc5763db83cac447
Gerard Darris <darrisgr@gmail.com>
2020-02-03 05:08:39 -0600

    Updated queries.sql

--- queries.sql
+++ queries.sql
@@ -1,7 +1,9 @@
 ## Part 1: Test it with SQL
-SHOW COLUMNS FROM job;
+-- SHOW COLUMNS FROM job; 
 
 ## Part 2: Test it with SQL
+-- SELECT name from techjobs.employer
+-- WHERE (location = "St. Louis") OR (location = "Saint Louis");
 
 ## Part 3: Test it with SQL
 
