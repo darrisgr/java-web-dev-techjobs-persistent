145572b8033b698d24613c46751eca09a5a9063d
Gerard Darris <darrisgr@gmail.com>
2020-02-03 20:23:41 -0600

    Updated queries.sql

--- queries.sql
+++ queries.sql
@@ -9,3 +9,9 @@
 -- DROP TABLE techjobs.job;
 
 ## Part 4: Test it with SQL
+
+-- SELECT techjobs.skill.name, techjobs.skill.description
+-- FROM skill
+-- LEFT JOIN job_skills ON skill.id = job_skills.skills_id
+-- WHERE jobs_id IS NOT NULL
+-- ORDER BY skill.name;
